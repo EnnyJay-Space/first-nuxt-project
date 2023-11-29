5e90cd5e9a8f2ab73c704bb91dcad369a6b9ee7c
ennyworld <ennyjay4you@gmail.com>
2023-11-29 10:35:58 +0100

    updated

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perspiciatis, excepturi esse in soluta fugiat. Mollitia sit odit impedit hic.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perspiciatis, excepturi esse in soluta fugiat. Mollitia sit odit impedit hic.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div class="grid grid-cols-4 gap-5">
+			<div v-for="p in products">
+				<ProductCard :product="p" />
+
+				<!-- <NuxtLink :to="`/products/${p.id}` " class="font-bold">{{ p.title }}</NuxtLink> -->
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+    definePageMeta({
+        layout: 'products'
+    })
+
+    // fetch the products
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+	// useHead({
+	// 	title: 'nuxt dojo | Merch',
+	// 	meta: [
+	// 		{name:'description', content:'Nuxt 3 Merch'}
+	// 	]
+	// })
+</script>
+
+<style scoped>
+
+</style>
